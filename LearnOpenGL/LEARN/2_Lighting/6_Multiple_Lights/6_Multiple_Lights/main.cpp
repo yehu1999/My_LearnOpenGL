d894848556ff2bbf9078f33ec69427f877721ec1
Ajga <114279940+yehu1999@users.noreply.github.com>
2023-10-29 13:25:52 +0800

    Add files via upload

---
+++ LearnOpenGL/LEARN/2_Lighting/6_Multiple_Lights/6_Multiple_Lights/main.cpp
@@ -0,0 +1,440 @@
+//运行此文件前，请将其他非依赖文件从生成中排除
+#include<mythings/shader.h>
+#include<mythings/camera.h>
+#include<stb_image.h>
+
+//settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+const char* vsPath = "E:\\Learn\\Deps\\GLSL\\2_Lighting\\6\\vShader.vs";
+const char* fsPath = "E:\\Learn\\Deps\\GLSL\\2_Lighting\\6\\fShader.fs";
+const char* L_vsPath = "E:\\Learn\\Deps\\GLSL\\2_Lighting\\6\\vLightShader.vs";
+const char* L_fsPath = "E:\\Learn\\Deps\\GLSL\\2_Lighting\\6\\fLightShader.fs";
+
+//摄像机
+Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));//相机初始位置
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+//时间
+float deltaTime = 0.0f; // 当前帧与上一帧的时间差
+float lastFrame = 0.0f; // 上一帧的时间
+
+//函数
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void controller(GLFWwindow* window);
+void FeedBack();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+unsigned int loadTexture(char const* path);//纹理加载函数
+
+int main()
+{
+	//调整一个好的摄像机初始视角//移动鼠标后后会跳视角至(0,0,-1)，暂时只能这样
+	//camera.Front = glm::vec3(0.604987, -0.538771, -0.586274);
+	//基础设置
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//窗口设置
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);//追踪并隐藏光标
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//注册窗口大小调整回调函数
+	glfwSetCursorPosCallback(window, mouse_callback);//注册鼠标回调函数(鼠标一移动注册的函数就会被调用)
+	glfwSetScrollCallback(window, scroll_callback);//注册滚轮回调函数
+
+	//glad加载
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//使用着色器函数创建着色器程序
+	Shader shader(vsPath, fsPath);
+	Shader LightShader(L_vsPath, L_fsPath);
+
+	//顶点数据(立方体)
+	float vertices[] =
+	{
+		// positions        // normals          // texture coords
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+	};
+	//10个立方体的世界位置数据
+	glm::vec3 cubePositions[] =
+	{
+	  glm::vec3(0.0f,  0.0f,  0.0f),
+	  glm::vec3(2.0f,  5.0f, -15.0f),
+	  glm::vec3(-1.5f, -2.2f, -2.5f),
+	  glm::vec3(-3.8f, -2.0f, -12.3f),
+	  glm::vec3(2.4f, -0.4f, -3.5f),
+	  glm::vec3(-1.7f,  3.0f, -7.5f),
+	  glm::vec3(1.3f, -2.0f, -2.5f),
+	  glm::vec3(1.5f,  2.0f, -2.5f),
+	  glm::vec3(1.5f,  0.2f, -1.5f),
+	  glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	//点光源位置
+	glm::vec3 pointLightPositions[] =
+	{
+	glm::vec3(0.7f,  0.2f,  2.0f),
+	glm::vec3(2.3f, -3.3f, -4.0f),
+	glm::vec3(-4.0f,  2.0f, -12.0f),
+	glm::vec3(0.0f,  0.0f, -3.0f)
+	};
+	//BOX
+	//创建VAO，VBO
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//绑定VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//将顶点数据通过VBO存进显卡
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//绑定VAO
+	glBindVertexArray(VAO);
+	//设置顶点属性指针
+	//位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	//法向量属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	//纹理(光照贴图)属性
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//Light
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	// 只需要绑定VBO不用再次设置VBO的数据，因为箱子的VBO数据中已经包含了正确的立方体顶点数据
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// 设置灯立方体的顶点属性（对我们的灯来说仅仅只有位置数据）
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//创建光照贴图对象
+	unsigned int diffuseMap = loadTexture("E:\\Learn\\Deps\\images_in\\container2.png");
+	unsigned int specularMap = loadTexture("E:\\Learn\\Deps\\images_in\\container2_specular.png");
+	unsigned int emissionMap = loadTexture("E:\\Learn\\Deps\\images_in\\container2_emission.jpg");
+
+	//启用深度测试
+	glEnable(GL_DEPTH_TEST);
+
+	//渲染循环
+	while (!glfwWindowShouldClose(window))
+	{
+		//激活场景的着色器
+		shader.use();
+
+		//设置视角方向
+		shader.setVec3("viewPos", camera.Position);
+
+		//设置物体材质
+		shader.setVec3("material.ambient", 0.24725f, 0.1995f, 0.31f);
+		//shader.setVec3("material.diffuse", 1.0f, 0.8745f, 0.31f);//漫反射颜色实际才是物体本身的颜色
+		//shader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+		shader.setFloat("material.shininess", 32.0f);
+		//将纹理单元赋值给材质的uniform采样器
+		shader.setTex("material.diffuse", 0);
+		shader.setTex("material.specular", 1);
+		//shader.setTex("material.emission", 2);
+		//激活材质单元并将对应材质绑定上去
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, specularMap);
+		//glActiveTexture(GL_TEXTURE2);
+		//glBindTexture(GL_TEXTURE_2D, emissionMap);
+
+		//设置光照
+		// directional light
+		shader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+		shader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+		shader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+		shader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+		// point light 1
+		shader.setVec3("pointLights[0].position", pointLightPositions[0]);
+		shader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+		shader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+		shader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+		shader.setFloat("pointLights[0].constant", 1.0f);
+		shader.setFloat("pointLights[0].linear", 0.09f);
+		shader.setFloat("pointLights[0].quadratic", 0.032f);
+		// point light 2
+		shader.setVec3("pointLights[1].position", pointLightPositions[1]);
+		shader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+		shader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+		shader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+		shader.setFloat("pointLights[1].constant", 1.0f);
+		shader.setFloat("pointLights[1].linear", 0.09f);
+		shader.setFloat("pointLights[1].quadratic", 0.032f);
+		// point light 3
+		shader.setVec3("pointLights[2].position", pointLightPositions[2]);
+		shader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+		shader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+		shader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+		shader.setFloat("pointLights[2].constant", 1.0f);
+		shader.setFloat("pointLights[2].linear", 0.09f);
+		shader.setFloat("pointLights[2].quadratic", 0.032f);
+		// point light 4
+		shader.setVec3("pointLights[3].position", pointLightPositions[3]);
+		shader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+		shader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+		shader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+		shader.setFloat("pointLights[3].constant", 1.0f);
+		shader.setFloat("pointLights[3].linear", 0.09f);
+		shader.setFloat("pointLights[3].quadratic", 0.032f);
+		// spotLight
+		shader.setVec3("spotLight.position", camera.Position);
+		shader.setVec3("spotLight.direction", camera.Front);
+		shader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+		shader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+		shader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+		shader.setFloat("spotLight.constant", 1.0f);
+		shader.setFloat("spotLight.linear", 0.09f);
+		shader.setFloat("spotLight.quadratic", 0.032f);
+		shader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+		shader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
+
+		//激活光源的着色器
+		LightShader.use();
+		LightShader.setVec3("LightColor", 1.0f, 1.0f, 1.0f);
+
+		// per-frame time logic
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		//输入检测
+		processInput(window);
+		//清屏
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//清除上一次的颜色和深度缓存
+
+		//BOX
+		//激活着色器
+		shader.use();
+		//---------------------------------
+		//模型矩阵
+		glm::mat4 model = glm::mat4(1.0f);
+		//model = glm::translate(model, boxPos);
+		//观察矩阵
+		glm::mat4 view;//看向(相机坐标，目标坐标，世界竖直向上的坐标)
+		view = camera.GetViewMatrix();
+		//投影矩阵
+		glm::mat4 projection = glm::mat4(1.0f);
+		projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		//----------------------------------
+		shader.setMat4fv("view", view, 1, GL_FALSE);
+		shader.setMat4fv("projection", projection, 1, GL_FALSE);
+		//shader.setMat4fv("model", model, 1, GL_FALSE);//(在for循环中)
+		//绘制
+		glBindVertexArray(VAO);
+		for (unsigned int i = 1; i < 11; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i - 1]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader.setMat4fv("model", model, 1, GL_FALSE);;
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		//Light
+		//激活着色
+		LightShader.use();
+		//---------------------------------
+		//模型矩阵（在绘制for循环中）
+		//观察矩阵（与BOX共用）
+		//投影矩阵（与BOX共用）
+		//----------------------------------
+		LightShader.setMat4fv("view", view, 1, GL_FALSE);
+		LightShader.setMat4fv("projection", projection, 1, GL_FALSE);
+		
+		//绘制
+		glBindVertexArray(lightVAO);
+		for (int i = 0; i < 4; i++)
+		{
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, pointLightPositions[i]);   //移动
+			model = glm::scale(model, glm::vec3(0.2f));//缩小
+
+			LightShader.setMat4fv("model", model, 1, GL_FALSE);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		//双缓冲
+		glfwSwapBuffers(window);
+		//事件检测
+		glfwPollEvents();
+		//运行反馈
+		//FeedBack();
+	}
+	std::cout << "MISSION COMPLETE!" << std::endl;
+	//结束，释放资源(VAO,VBO,着色器程序)
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &VBO);
+	shader.del();
+	LightShader.del();
+
+	//终止，清除所有先前分配的 GLFW 资源。
+	glfwTerminate();
+
+	return 0;
+}
+
+void controller(GLFWwindow* window)
+{
+	//ESC
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	//摄像机：WASD
+	Camera_Movement direction;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	// 注意这里y偏移量是相反的，因为y坐标是从底部往顶部依次增大的
+	// 我再解释一下为什么是相反的，记住视线方向是View_z轴负方向
+	// 如果不取反，Screen_y偏移量增加，pitch增加，上抬的是原本是z轴方向的向量，其反方向才是视线，也就是视线实际在下压
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+	//controller
+	controller(window);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+void FeedBack()
+{
+	camera.report();
+}
+
+unsigned int loadTexture(char const* path)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
